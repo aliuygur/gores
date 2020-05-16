2d0195ec19fc07125ea89767e47d9baca6e38322
Ali OYGUR <ali.oygur@metglobal.com>
2020-05-17 01:29:40 +0300

    added must-pattern

--- gores.go
+++ gores.go
@@ -78,23 +78,37 @@ func String(w http.ResponseWriter, code int, s string) {
 }
 
 // JSON sends a JSON response with status code.
-func JSON(w http.ResponseWriter, code int, i interface{}) (err error) {
+func JSON(w http.ResponseWriter, code int, i interface{}) error {
 	b, err := json.Marshal(i)
 	if err != nil {
 		return err
 	}
 	_json(w, code, b)
-	return
+	return nil
+}
+
+// MustJSON calls JSON and panics on error
+func MustJSON(w http.ResponseWriter, code int, i interface{}) {
+	if err := JSON(w, code, i); err != nil {
+		panic(err)
+	}
 }
 
 // JSONIndent sends a JSON response with status code, but it applies prefix and indent to format the output.
-func JSONIndent(w http.ResponseWriter, code int, i interface{}, prefix string, indent string) (err error) {
+func JSONIndent(w http.ResponseWriter, code int, i interface{}, prefix string, indent string) error {
 	b, err := json.MarshalIndent(i, prefix, indent)
 	if err != nil {
 		return err
 	}
 	_json(w, code, b)
-	return
+	return nil
+}
+
+// MustJSONIndent calls JSONIndent and panics on error
+func MustJSONIndent(w http.ResponseWriter, code int, i interface{}, prefix string, indent string) {
+	if err := JSONIndent(w, code, i, prefix, indent); err != nil {
+		panic(err)
+	}
 }
 
 func _json(w http.ResponseWriter, code int, b []byte) {
@@ -105,7 +119,7 @@ func _json(w http.ResponseWriter, code int, b []byte) {
 
 // JSONP sends a JSONP response with status code. It uses `callback` to construct
 // the JSONP payload.
-func JSONP(w http.ResponseWriter, code int, callback string, i interface{}) (err error) {
+func JSONP(w http.ResponseWriter, code int, callback string, i interface{}) error {
 	b, err := json.Marshal(i)
 	if err != nil {
 		return err
@@ -115,27 +129,48 @@ func JSONP(w http.ResponseWriter, code int, callback string, i interface{}) (err
 	w.Write([]byte(callback + "("))
 	w.Write(b)
 	w.Write([]byte(");"))
-	return
+	return nil
+}
+
+// MustJSONP calls JSONP and panics on error
+func MustJSONP(w http.ResponseWriter, code int, callback string, i interface{}) {
+	if err := JSONP(w, code, callback, i); err != nil {
+		panic(err)
+	}
 }
 
 // XML sends an XML response with status code.
-func XML(w http.ResponseWriter, code int, i interface{}) (err error) {
+func XML(w http.ResponseWriter, code int, i interface{}) error {
 	b, err := xml.Marshal(i)
 	if err != nil {
 		return err
 	}
 	_xml(w, code, b)
-	return
+	return nil
+}
+
+// MustXML calls XML and panics on error
+func MustXML(w http.ResponseWriter, code int, i interface{}) {
+	if err := XML(w, code, i); err != nil {
+		panic(err)
+	}
 }
 
 // XMLIndent sends an XML response with status code, but it applies prefix and indent to format the output.
-func XMLIndent(w http.ResponseWriter, code int, i interface{}, prefix string, indent string) (err error) {
+func XMLIndent(w http.ResponseWriter, code int, i interface{}, prefix string, indent string) error {
 	b, err := xml.MarshalIndent(i, prefix, indent)
 	if err != nil {
 		return err
 	}
 	_xml(w, code, b)
-	return
+	return nil
+}
+
+// MustXMLIndent calls XMLIndent and panics on error
+func MustXMLIndent(w http.ResponseWriter, code int, i interface{}, prefix string, indent string) {
+	if err := XMLIndent(w, code, i, prefix, indent); err != nil {
+		panic(err)
+	}
 }
 
 func _xml(w http.ResponseWriter, code int, b []byte) {
@@ -146,16 +181,28 @@ func _xml(w http.ResponseWriter, code int, b []byte) {
 }
 
 // File sends a response with the content of the file
-func File(w http.ResponseWriter, r *http.Request, path string) (err error) {
-	err = file(w, r, path, "", false)
-	return
+func File(w http.ResponseWriter, r *http.Request, path string) error {
+	return file(w, r, path, "", false)
+}
+
+// MustFile calls File and panics on error
+func MustFile(w http.ResponseWriter, r *http.Request, path string) {
+	if err := File(w, r, path); err != nil {
+		panic(err)
+	}
 }
 
 // Download the client is prompted to save the file with provided `name`,
 // name can be empty, in that case name of the file is used.
-func Download(w http.ResponseWriter, r *http.Request, path string, name string) (err error) {
-	err = file(w, r, path, name, true)
-	return
+func Download(w http.ResponseWriter, r *http.Request, path string, name string) error {
+	return file(w, r, path, name, true)
+}
+
+// MustDownload calls Download and panics on error
+func MustDownload(w http.ResponseWriter, r *http.Request, path string, name string) {
+	if err := Download(w, r, path, name); err != nil {
+		panic(err)
+	}
 }
 
 func file(w http.ResponseWriter, r *http.Request, path, name string, attachment bool) (err error) {
